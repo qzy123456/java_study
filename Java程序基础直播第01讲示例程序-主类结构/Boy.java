ac53a300aad9bcd16a64b349884ff02bacc39db5
qichaoyang <chaoyang.qi@mybogames.com>
2020-09-04 11:22:23 +0800

    java

--- Java程序基础直播第01讲示例程序-主类结构/Boy.java
+++ Java程序基础直播第01讲示例程序-主类结构/Boy.java
@@ -1,14 +1,12 @@
 import java.awt.*;
-import java.applet.Applet;
+import java.applet.*;
 
 public class Boy extends Applet{
    public void paint(Graphics g){
-g.setColor(Color.red);      
-g.drawString("学习Java有什么捷径呢？", 10, 20);
+	g.setColor(Color.red);      
+	g.drawString("hello java", 10, 20);
 
-g.setColor(Color.blue);      
-g.drawString("为什么Java图标是一杯咖啡 ", 15, 35);
-
-
+	g.setColor(Color.blue);      
+	g.drawString("hello ", 15, 35);
     }
 }
--- Java程序基础直播第01讲示例程序-主类结构/HelloB.java
+++ Java程序基础直播第01讲示例程序-主类结构/HelloB.java
@@ -1,7 +1,7 @@
 // Hello.java
 public class Hello{
     public static void main(String[ ] args){
-        System.out.println("ÄãºÃJava!");
+        System.out.println("Hello Java!");
   }
  }
 
