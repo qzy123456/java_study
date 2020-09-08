baa5325a2ae74058a64f2989e553dc5c3ad5e1d8
qichaoyang <chaoyang.qi@mybogames.com>
2020-09-08 19:58:04 +0800

    挂机系统

---
+++ Java02/ComputeArea2.java
@@ -0,0 +1,15 @@
+import java.math.*;
+public class ComputeArea2 {  //定义类
+	 	 
+	 public static void main(String[] args) {
+	    double wide;        //定义变量，保存宽
+	    double height;       //定义变量，保存高
+		 double area;       //面积
+		 wide = 5;
+		 height = 10;
+        area = wide * height;  //计算面积
+	    System.out.println("面积为:"  + area); //打印结果
+	       
+	  }	
+	 
+}
